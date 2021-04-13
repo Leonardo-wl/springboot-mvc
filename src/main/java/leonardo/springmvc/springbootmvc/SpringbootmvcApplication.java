a3306e90273708062372ac798bccc697c168ac52
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-04-13 03:04:03 -0300

    Primeiro commit

---
+++ src/main/java/leonardo/springmvc/springbootmvc/SpringbootmvcApplication.java
@@ -0,0 +1,13 @@
+package leonardo.springmvc.springbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leonardo/springmvc/springbootmvc/SpringbootmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package leonardo.springmvc.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
